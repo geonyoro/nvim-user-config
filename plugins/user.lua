9c5268f48dd7199f15153fe07b16b294d8f33f50
George Nyoro <geonyoro@gmail.com>
2024-02-21 08:07:23 +0300

    feat: Install venv-selector

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,12 +2,25 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   {
+    'linux-cultist/venv-selector.nvim',
+    opts = {
+      name = {"venv", ".venv", "env", ".env"}
+    },
+    event = 'VeryLazy',
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
+  },
+  {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
-{
+  {
     "tpope/vim-fugitive",
     keys = {
       { "<leader>gs", ":Git status<CR>", desc = "Git Status" },
